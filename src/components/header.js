e70de70bebc428006fa7a7d817cf0b11a4729bea
John Quealy <john@volary.org>
2014-07-29 12:44:06 -0700

    Header.

--- src/components/header.js
+++ src/components/header.js
@@ -23,14 +23,16 @@ var Header = React.createClass({
   },
 
   render: function() {
-    return d.div({ className: 'container', onClick: this.toPage }, 
-      d.a({ rel: 'home' }, 'home'),  
-      d.a({ rel: 'about' }, 'about'),  
-      d.a({ rel: 'media' }, 'media'),
-      d.a({ rel: 'partners' }, 'partners')  
+    return d.div({ className: 'page-header text-center', },
+      d.h1({ className: 'name',}, 'Awaren.es ', d.small({ className: 'tagline',}, 'The Awareness Campaign Builder')),
+      d.ul({ className: 'container nav nav-pills', onClick: this.toPage },
+        d.li({}, d.a({ rel: 'home' }, 'home')),
+        d.li({}, d.a({ rel: 'about' }, 'about')),
+        d.li({}, d.a({ rel: 'media' }, 'media')),
+        d.li({}, d.a({ rel: 'partners' }, 'partners'))
+      )
     )
   }
 })
 
 module.exports = Header
-

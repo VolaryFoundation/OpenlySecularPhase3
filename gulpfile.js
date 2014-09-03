d7c047a85342b47b90bde1ccc7fb275e2af5daa4
bbarr <bbarr1384@gmail.com>
2014-09-03 14:43:52 -0700

    remove npm parse and update gulpfil to use parse

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp')
 var mongo = require('mongo-promise')
 var config = require('config')
+var Parse = require('parse')
 
 var srcPath = __dirname + '/src'
 var builder = require('volary-static-builder')({ srcPath: srcPath })
@@ -9,126 +10,139 @@ var builder = require('volary-static-builder')({ srcPath: srcPath })
 mongo.url = config.database.url
 mongo.shortcut('campaigns')
 
+
 gulp.task('buildAndServe', function(done) {
-  mongo.campaigns.find({ slug: 'development' }, {}).then(function(found) {
-    var campaign = found[0]
-    console.log('serving ', campaign._id)
-    builder.buildAndServe({ campaign: { id: campaign._id, slug: campaign.slug } }, {})
-    done()
-  }).catch(function() { console.log(arguments) })
+
+  Parse.initialize(config.parse.appId, config.parse.jsKey);
+
+  var Campaign = Parse.Object.extend('Campaign')
+  var query = new Parse.Query(Campaign)
+  query
+    .equalTo('slug', 'development')
+    .first(function(campaign) {
+      builder.buildAndServe({ campaign: campaign.toJSON() }, {})
+      done()
+    })
 })
 
 gulp.task('initDev', function(done) {
-  mongo.campaigns.update({
-    slug: 'development'
-  }, {
-    userId: "MF8y1308Qh",
-    about1: {
-      content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
-      title: "Our Mission blah",
-    },
-    about2: {
-      content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
-      title: "Our Mission blah",
-    },
-    about3: {
-      content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
-      title: "Our Mission blah",
-    },
-    about4: {
-      content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
-      title: "Our Mission blah",
-    },
-    DIY: {
-      content: "<strong>Blah is bold</strong><br>shnarf",
-    },
-    logo: "https://richarddawkins.net/file/2014/06/Openly-Secular-logo-2C-RGB-700x700.jpg",
-    partners: [
-      {
-        _id: 0,
-        title: 'friends',
-        description: 'some friends of ours',
-        list: [
+
+  Parse.initialize(config.parse.appId, config.parse.jsKey);
+
+  var Campaign = Parse.Object.extend('Campaign')
+  var query = new Parse.Query(Campaign)
+  var c = query
+    .equalTo('slug', 'development')
+    .first(function(campaign) {
+      campaign.set({
+        userId: "MF8y1308Qh",
+        about1: {
+          content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
+          title: "Our Mission blah",
+        },
+        about2: {
+          content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
+          title: "Our Mission blah",
+        },
+        about3: {
+          content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
+          title: "Our Mission blah",
+        },
+        about4: {
+          content: "Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.",
+          title: "Our Mission blah",
+        },
+        DIY: {
+          content: "<strong>Blah is bold</strong><br>shnarf",
+        },
+        logo: "https://richarddawkins.net/file/2014/06/Openly-Secular-logo-2C-RGB-700x700.jpg",
+        partners: [
           {
             _id: 0,
-            name: "rdf",
-            logo: "someogo.png",
-            link: 'rdf.net'
-          }
-        ]
-      },
-      {
-        _id: 1,
-        title: 'people who hate us',
-        description: 'some friends of ours... not',
-        list: [
+            title: 'friends',
+            description: 'some friends of ours',
+            list: [
+              {
+                _id: 0,
+                name: "rdf",
+                logo: "someogo.png",
+                link: 'rdf.net'
+              }
+            ]
+          },
           {
-            _id: 0,
-            name: "brendan",
-            logo: "someogo.png",
-            link: 'google.com'
+            _id: 1,
+            title: 'people who hate us',
+            description: 'some friends of ours... not',
+            list: [
+              {
+                _id: 0,
+                name: "brendan",
+                logo: "someogo.png",
+                link: 'google.com'
+              }
+            ]
           }
-        ]
-      }
-    ],
-    contact: {
-      name: "Campaign Name",
-      address: "1234 Street, City Sate Zip",
-      ein: '55555555555555',
-      email: 'contact@campaign.org',
-      phone: '555-555-5555',
-      fax: '555-555-5555',
-      pressName: 'Contact Name',
-      pressEmail: 'press@campaign.org',
-      pressPhone: '555-555-5555',
-      twitter: '@campaign',
-      instagram: 'campaign',
-      youtube: 'campaign'
-    },
-    downloads: {
-      title: 'Downloads',
-      description: 'Get your files bitch',
-      list: [
-        {
-          _id: 0,
-          name: "rdf",
-          file: "someogo.png",
-          description: 'This is my description babyy'
-        }
-      ]
-    },
-    updates: {
-      title: 'Latest updates',
-      list: [
-        { _id: 0, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
-        { _id: 1, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
-        { _id: 2, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
-        { _id: 3, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
-        { _id: 4, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
-        { _id: 5, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' }
-      ]
-    },
-    news: {
-      title: 'In the news',
-      list: [
-        { _id: 0, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 1, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 2, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 3, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 4, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 5, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
-        { _id: 6, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' }
-      ]
-    },
-    resources: {
-      title: 'Resources',
-      list: [
-        { _id: 0, title: 'Freethinkers Whatever', desc: 'This is a descritpion more than a source', link: 'http://nytimes.com' }
-      ]
-    },
-    slug: "development",
-    title: "Some freakin campaign"
-  }, { upsert: true }).then(done, done)
+        ],
+        contact: {
+          name: "Campaign Name",
+          address: "1234 Street, City Sate Zip",
+          ein: '55555555555555',
+          email: 'contact@campaign.org',
+          phone: '555-555-5555',
+          fax: '555-555-5555',
+          pressName: 'Contact Name',
+          pressEmail: 'press@campaign.org',
+          pressPhone: '555-555-5555',
+          twitter: '@campaign',
+          instagram: 'campaign',
+          youtube: 'campaign'
+        },
+        downloads: {
+          title: 'Downloads',
+          description: 'Get your files bitch',
+          list: [
+            {
+              _id: 0,
+              name: "rdf",
+              file: "someogo.png",
+              description: 'This is my description babyy'
+            }
+          ]
+        },
+        updates: {
+          title: 'Latest updates',
+          list: [
+            { _id: 0, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
+            { _id: 1, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
+            { _id: 2, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
+            { _id: 3, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
+            { _id: 4, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' },
+            { _id: 5, title: 'Our monthly status', date: '04/04/04', excerpt: 'This is a short excerpt...', content: 'Loren gotsum, boy!, dolor sit amet, consectetur adipiscing elit. Proin pharetra lectus ut rhoncus suscipit. Sed et elit sit amet velit tincidunt volutpat vitae id eros. Nullam tincidunt sollicitudin mauris, consectetur faucibus lorem dignissim vel.' }
+          ]
+        },
+        news: {
+          title: 'In the news',
+          list: [
+            { _id: 0, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 1, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 2, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 3, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 4, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 5, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' },
+            { _id: 6, title: 'news article 1', date: '04/04/04', source: 'nytimes.com', link: 'http://nytimes.com' }
+          ]
+        },
+        resources: {
+          title: 'Resources',
+          list: [
+            { _id: 0, title: 'Freethinkers Whatever', desc: 'This is a descritpion more than a source', link: 'http://nytimes.com' }
+          ]
+        },
+        slug: "development",
+        title: "Some freakin campaign"
+      }).save().then(done, done)
+    })
 })
 
 gulp.task('dev', [ 'buildAndServe' ], function() {
